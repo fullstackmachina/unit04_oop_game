ea2fc6cc071046f3a7766df23655c17454d9468c
fullstackmachina <misscyborgtech@gmail.com>
2026-02-16 15:01:01 -0500

    first commit

---
+++ js/Phrase.js
@@ -0,0 +1,22 @@
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase();
+  }
+
+  addPhraseToDisplay() {
+    const ul = document.querySelector("#phrase ul");
+    ul.innerHTML = "";
+    for (let character of this.phrase) {
+      let li = document.createElement("li");
+      if (character === " ") {
+        li.classList.add("space");
+      } else {
+        li.classList.add("hide", "letter", character);
+        li.textContent = character;
+      }
+      ul.appendChild(li);
+    }
+  }
+
+  checkLetter() {}
+}
